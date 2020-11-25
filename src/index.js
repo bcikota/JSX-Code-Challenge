8a62c9be20cec3ec4017b2f09ea770d68ffae06c
bcikota <48676010+bcikota@users.noreply.github.com>
2020-11-25 14:45:52 +0100

    challange

--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
 //Create a react app from scratch.
+import React from "react";
+import ReactDOM from "react-dom";
 //It should display a h1 heading.
 //It should display an unordered list (bullet points).
 //It should contain 3 list elements.
+
+ReactDOM.render(
+  <div>
+    <h1>To Do List</h1>
+    <ul>
+      <li>Buy food</li>
+      <li>Cook food</li>
+      <li>Eat food</li>
+    </ul>
+  </div>,
+  document.querySelector("#root")
+);
